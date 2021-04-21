6a44e5a53ee5eb9fd1d272824b18831da14d0fb8
Gina.Chen/陳靓 <Gina.Chen@JMC.LOCAL>
2021-04-21 15:19:09 +0800

    add npm packages lodash and live-server

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+var _ = require('lodash');
 
 
 // Write code so that the colour inputs match the background generated on the first page load. 
 // Display the initial CSS linear gradient property on page load.
 // BONUS: Add a random button which generates two random numbers for the colour inputs.
 
+var array = [1,2,3,4,5,6,7,8];
+console.log('answer:', _.without(array, 3));
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
